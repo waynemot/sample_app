e686b42f22900a2f5390722a018cddb6c894229c
Wayne Motycka <wmotycka@cse.unl.edu>
2018-12-21 13:48:57 -0600

    add signup controller/view-new end-of-ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/bugle'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/bugle', to: 'static_pages#bugle'
+  get '/contacts', to: 'static_pages#contacts'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
