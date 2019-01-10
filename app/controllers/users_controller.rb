1b9f1535ee688bd364729a2caf433287c95f898e
Wayne Motycka <wmotycka@cse.unl.edu>
2019-01-10 09:58:06 -0600

    mod users controller for new User and create methods of signup
    Signed-off-by: Wayne Motycka <wmotycka@cse.unl.edu>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
@@ -8,5 +9,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
